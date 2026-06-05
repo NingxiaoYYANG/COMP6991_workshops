8e281ee4003ab86fcf6ffb097032c669aeb84d3b
NingxiaoYYANG <justinycareer@gmail.com>
2026-06-05 15:52:23 +1000

    ws01

--- ws01/src/main.rs
+++ ws01/src/main.rs
@@ -1,118 +1,157 @@
-
-use bmp::{Image, Pixel};
+// Rust basics
 
 fn main() {
-    let path = std::env::args().nth(1).expect("You must provide a path.");
-    let operation = std::env::args().nth(2).expect("You must provide an operation.");
+    /* 
+    Basic types in rust
+    integer: i32, i8, i16, u32, u8, u16, isize, usize
+    floating-point: f32, f64
+    boolean: bool
+    character: char
+    string: &str(read only), String(read, write, update)
 
-    if operation.as_str() == "pixel" {
-        draw_pixel(path.as_str());
-    } else if operation.as_str() == "something_else" {
-        // Add more cases here!
-    } else {
-        eprintln!("The operation {operation} was not recognised!");
-    }
+    Stack: &str (Static size. fixed)
 
-    match operation.as_str() {
-        "pixel" => {
-            draw_pixel(path.as_str());
-        },
-        "something_else" => todo!(),
-        _ => eprintln!("The operation {operation} was not recognised!"),
-    }
+    Heap: String (Dynaimic size. Modifiable)
+    */ 
+    
+    // declare a variable
+    // let mut my_var: i32 = 42;
+    // my_var += 1;
+    // println!("{}", my_var.abs());
+
+
+    // if statement
+    // let is_even = if 2 % 2 == 0 {
+    //     true
+    // } else {
+    //     false
+    // };
+
+    
+
+    // for loop;
+    // for (int i = 0; i < n; i++) {
+    //     v[i]
+    // }
+
+    // let v = vec![1, 2, 3];
+
+    // for num in v {
+    //     println!("{}", num);
+    // }
+
+    // println!("{}", is_even(5));
+
 }
 
-fn draw_pixel(path: &str) {
-    let mut image = Image::new(100, 100);
-    image.set_pixel(50, 50, Pixel::new(255, 255, 255));
-    image.save(path).expect("This should save correctly.");
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// // Rust basics
-
-// fn main() {
-//     /* 
-//     Basic types in rust
-//     integer: i8, i16, i32, u8, u16, u32, isize, usize
-//     floating-point: f32, f64
-//     boolean: bool
-//     character: char
-//     string: &str (only read)(Stack), String (read, write, update)(Heap)
-
-//     Stack:
-//     array: static memory size
-
-//     Heap:
-//     arrayList(Java): Dynamic memory size
-//     */ 
-    
-//     // declare a variable
-//     // let mut myVar = 5;
-//     // myVar += 1; 
-
-
-//     // if statement
-//     // if 2 % 2 == 0 {
-//     //     println!("Hello");
-//     // } else if 2 % 2 == 1 {
-//     //     println!("Hi");
-//     // }
-
-//     // for loop
-    
-//     // let vec = vec![1, 2, 3];
-//     // for num in vec {
-//     //     println!("curr num: {}", num);
-//     // }
-
-//     let first = foo(11);
-//     let second= foo(9);
-
-//     println!("First: {}, Second: {}", first, second);
-
+// declare function
+// all functions are expressions
+// fn is_even(n: i32) -> bool {
+//     if n % 2 == 0 {
+//         return true;
+//     }
+//     false
 // }
 
-// // declare function
-// fn foo(n: i32) -> i32 {
-//     if n > 10 {
-//         n
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// use bmp::{Image, Pixel};
+
+// fn main() {
+//     let path = std::env::args().nth(1).expect("You must provide a path.");
+//     let operation = std::env::args().nth(2).expect("You must provide an operation.");
+
+//     if operation.as_str() == "pixel" {
+//         draw_pixel(path.as_str());
+//     } else if operation.as_str() == "something_else" {
+//         // Add more cases here!
 //     } else {
-//         10
+//         eprintln!("The operation {operation} was not recognised!");
 //     }
-// } 
+
+//     match operation.as_str() {
+//         "pixel" => {
+//             draw_pixel(path.as_str());  
+//         },
+//         "something_else" => todo!(),
+//         _ => eprintln!("The operation {operation} was not recognised!"),
+//     }
+// }
+
+// fn draw_pixel(path: &str) {
+//     let mut image = Image::new(100, 100);
+//     image.set_pixel(50, 50, Pixel::new(255, 255, 255));
+//     image.save(path).expect("This should save correctly.");
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
