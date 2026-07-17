4702a26a320019cd0e73908dae66a4ffffbf88a5
NingxiaoYYANG <justinycareer@gmail.com>
2026-07-17 16:59:32 +1000

    week7 finished

--- week7/demo7/src/main.rs
+++ week7/demo7/src/main.rs
@@ -1,31 +1,62 @@
-fn add_one(x: i32) -> i32 {
-    x + 1
+macro_rules! greet {
+    ($x:expr) => {
+        println!("{}", $x);
+    };
 }
 
-fn apply_function(f: TODO, x: i32) -> i32 {
+macro_rules! print_many {
+    ($($x:expr),* $(,)?) => {
+        $(
+            println!("{}", $x);
+        )*
+    };
+}
+
+fn double_one(x: i32) -> i32 {
+    x * 2
+}
+
+fn apply_function(f: fn(i32) -> i32, x: i32) -> i32 {
     f(x)
 }
 
-fn call_with_fn(f: TODO, x: i32) -> i32 
+fn call_with_fn<F: Fn(i32) -> i32>(f: F, x: i32) -> i32 
 {
     f(x)
 }
 
-fn call_with_fn_mut(mut f: TODO, x: i32) -> i32 {
+fn call_with_fn_mut<F: FnMut(i32) -> i32> (f: &mut F, x: i32) -> i32 {
     f(x)
 }
 
 
-fn call_with_fn_once(f: TODO, x: i32) -> String {
+fn call_with_fn_once(f: impl FnOnce(i32) -> String, x: i32) -> String {
     f(x)
 }
+
+
 
 /*
-fn: 
+fn: Function pointers, concret type
 
-Fn: 
-FnMut: 
-FnOnce: 
+closure: 
+Function without name, can capture environment variables
+
+Closure Traits:
+Fn: Closure that captures environment through &T, Can called multiple times
+FnMut: Closure that captures environment through &mut T, Can called multiple times
+FnOnce: Closure that captures environment through T, Can only called once
+
+Metaprogramming:
+Write code that manipulate or generates code
+
+Macros:
+
+Declartive Macro: ! (println!, dbg!, vec!)
+A pattern matching way to match code token fragments, then produce code expressions
+
+Procedure Macro: #[] (#[derive(...)], #[cfg(test)], #[attribute])
+
 */
 
 
@@ -33,13 +64,22 @@ fn main() {
     //// what is the type of a function we can pass around?
 
     // let f = add_one;
-    // println!("add_one(41) = {}", apply_function(f, 41));
+    // // f(3);
+    // println!("add_one(41) = {}", apply_function(double_one, 41));
 
     //// what if the callable needs to capture something from its environment?
 
-    // let x = 3;
-    // let add_x = |y| y + x; // captures x into closure
-    // println!("closure result = {}", call_with_fn(add_x, 4));
+    // let v = vec![1, 2, 3];
+
+    // println!("{:?}", v.iter().map(|x| x * 2).collect::<Vec<i32>>());    
+
+    // let x = 3 + 4 * 2;
+    // let add_x = |y| {
+    //     y + x
+    // }; // captures x into closure
+
+    // println!("{}", apply_function(add_x, 4));
+    // println!("closure result = {}", call_with_fn(add_one, 4));
 
     //// What if the closure needs to change something when it is called?
 
@@ -48,6 +88,7 @@ fn main() {
     //     total += value;
     //     total
     // };
+    // // let my_var = total; //
     // println!("first call = {}", call_with_fn_mut(&mut add_and_store, 2));
     // println!("second call = {}", call_with_fn_mut(&mut add_and_store, 3));
 
@@ -55,14 +96,37 @@ fn main() {
 
     // let message = String::from("Ada");
     // let consume_message = move |value: i32| {
-    //     format!("{} says {value}", message);  
+    //     format!("{} says {value}", message);
+    //     drop(message);
+    //     // message += "hello";
+    //     "Hello".to_string()
     // };
-    // println!("{}", call_with_fn_once(consume_message, 7));
+    // consume_message(7);
+    // consume_message(7);
+    // println!("{}", call_with_fn_once(&consume_message, 7));
+    // dbg!(message);
+    // println!("{}", call_with_fn_once(&consume_message, 7));
 
     // Finally, compare this with macros.
     // A macro does not work on values at runtime; it works on syntax and expands to code.
     
-    // greet!("Bob");
-    // print_many!("one" => 2.2 => 3.14);
+    // greet!(3 + 4 + 5);
+    // print_many!("one", 2, 3.14,);
 }
 
+/*
+Fragments Type:
+ident: identity, the name of variable or function
+expr: expression
+ty: type
+block: block expression, expr in {}
+literal: _
+tt: 
+
+*/
+
+
+
+// fn add_one(x: i32) -> i32 {
+//     x + 1
+// }
