e2e7fef481074e8cba09d0f9350d69e41d690b3e
NingxiaoYYANG <justinycareer@gmail.com>
2026-07-23 22:45:17 +1000

    week8 ready

--- week8/demo8/src/main.rs
+++ week8/demo8/src/main.rs
@@ -1,3 +1,34 @@
+/*
+Concurrency:
+
+Concurrency vs Parallelism
+
+Issues that could occur in concurrency:
+- Data Race
+- Dead Lock
+- Dangling Reference
+
+
+How other languages solve concurrency issues:
+C++:
+Java:
+Python:
+
+Thread Management:
+- spawn a thread
+- use thread scope to manage lifetime of each thread
+- chunks method
+
+How do we communicate between threads?
+- Send & Sync Trait
+- Channel
+    mpsc
+- Arc
+- Mutex
+
+
+*/
+
 fn main() {
     println!("Hello, world!");
 }
